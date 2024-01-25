3b5e4d93c86a121dfcf9cf85de9b85b684a59e39
Pacifista00 <adamsamudera99@gmail.com>
2024-01-25 17:26:50 +0700

    design database, create db factory & seed

---
+++ app/Models/Resident.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Resident extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'nik',
+        'name',
+        'gender',
+        'birthplace',
+        'birthdate',
+        'address',
+        'religion',
+        'profession',
+    ];
+
+
+}
---
+++ database/factories/ResidentFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class ResidentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'nik' => fake()->nik(),
+            'name' => fake()->name(),
+            'gender' => fake()->randomElement(['Male', 'Female']),
+            'birthplace' => $this->faker->city,
+            'birthdate' => $this->faker->dateTime('-17 years'),
+            'address' => $this->faker->address,
+            'city' => $this->faker->city,
+            'religion' => fake()->randomElement(['Islam', 'Kristen', 'Hindu', 'Buddha']),
+            'profession' => fake()->jobTitle,
+        ];
+    }
+}
