997f4df17cdc1917153b707d63251ead40f1a45a
Pacifista00 <adamsamudera99@gmail.com>
2024-01-28 02:18:21 +0700

    design view home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,41 @@
 @extends('./layouts/main')
 @section('view')
     @include('./partials/navbar')
+    <div class="container pt-4">
+        <div class="card rounded-4 p-3">
+            <div class="card-body">
+                <div class="my-card-header d-flex">
+                  <h3>DATA KTP</h3>
+                  <div class="search-bar mb-3 ms-auto">
+                      <form class="d-flex" role="search">
+                          <input class="form-control me-2 rounded-start-pill" type="search" placeholder="Search" aria-label="Search">
+                          <button class="btn my-btn rounded-end-pill" type="submit" style="width: auto;">Search</button>
+                      </form>
+                  </div>
+                </div>
+                <div class="export-section d-flex">
+                    <a href="" class="btn my-btn me-3"><i class="fa-solid fa-file-pdf me-1"></i>Export PDF</a>
+                    <a href="" class="btn my-btn"><i class="fa-solid fa-file-csv me-1"></i>Export CSV</a>
+                </div>
+                <table class="table mt-3">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">First</th>
+                            <th scope="col">Last</th>
+                            <th scope="col">Handle</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,23 +1,27 @@
-<nav class="navbar navbar-expand-lg my-bg-yellow">
+<nav class="navbar navbar-expand-lg my-bg-blue">
     <div class="container">
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav mx-auto">
+        <a class="title navbar-brand text-light" href="#">KTP MANAGER</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse " id="navbarSupportedContent">
+            <ul class="navbar-nav mb-2 mb-lg-0 ms-auto">
                 <li class="nav-item">
-                    <a class="nav-link p-3" href="#">Home</a>
+                    <a class="fs-6 nav-link mx-1" href="#">DATA KTP</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link p-3" href="#">Features</a>
+                    <a class="fs-6 nav-link mx-1" href="#">AKTIVITAS</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link p-3" href="#">Pricing</a>
+                    <a class="fs-6 nav-link mx-1" href="#">IMPORT</a>
+                </li>
+                <li class="nav-item">
+                    <form action="/logout" method="POST">
+                        @csrf
+                        <button type="submit" class="btn my-btn2 mx-1 px-3 rounded-pill" style="width: 100%;">LOGOUT</button>
+                    </form>
                 </li>
             </ul>
-            <div class="d-flex">
-                <form action="/logout" method="POST">
-                    @csrf
-                    <button type="submit" class="btn p-3">Logout</button>
-                </form>
-            </div>
         </div>
     </div>
 </nav>
